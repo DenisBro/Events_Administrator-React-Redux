e3ef661e6f6f74d3a1eda1aa173561a6b7b46ed7
denis <crasnov.d@gmail.com>
2017-05-11 16:40:22 +0200

    done static part of project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Events from './componets/Events';
+
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="wrap">
+        <div className="create-events">
+          <input type="text" placeholder="Name of event"/>
+          <input type="text" placeholder="Partisipation fee"/>
+          <input type="number" placeholder="Max number of participants"/>
+          <button>Create</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Events />
+
       </div>
     );
   }
